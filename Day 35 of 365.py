057bcd030a491ebcbb5509ee3e5c300121a39586
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-02 09:46:41 +0530

    Day 35 of 365 days of Python and ML
    Find Digits

--- Day 35 of 365.py
+++ Day 35 of 365.py
@@ -5,3 +5,20 @@ Created on Wed Jun  2 09:34:05 2021
 @author: Atul Mishra
 """
 
+'''
+Find Digits
+
+https://www.hackerrank.com/challenges/find-digits/problem
+'''
+
+n = 124
+my_list = [int(x) for x in str(n)]
+count = 0
+
+for num in my_list:
+    if num == 0:
+        pass
+    elif n%num == 0:
+        count +=1
+
+print(count)
