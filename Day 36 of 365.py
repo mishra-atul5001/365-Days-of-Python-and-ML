c80972a315cf3119f217f3b3f5fe79c4d48607c2
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-03 09:19:24 +0530

    Day 36 of 365 days of Python and ML
    Extra Long Factorials
    
    Calculate and print the factorial of a given integer.

--- Day 36 of 365.py
+++ Day 36 of 365.py
@@ -5,3 +5,20 @@ Created on Wed Jun  2 09:34:16 2021
 @author: Atul Mishra
 """
 
+'''
+Extra Long Factorials
+
+Calculate and print the factorial of a given integer. 
+'''
+
+def Extra_Long_Factorials(n):
+    prod = 1
+    for i in range(1,n+1):
+        prod = prod*i
+    return prod
+
+print(Extra_Long_Factorials(25),'\n')
+print(Extra_Long_Factorials(12),'\n')
+print(Extra_Long_Factorials(111),'\n')
+print(Extra_Long_Factorials(650),'\n')
+print(Extra_Long_Factorials(9))
