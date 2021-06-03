1026df5be368d7d4edc398dc2c6efd9b82187004
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-03 09:38:42 +0530

    Update Day 37 of 365.py

--- Day 37 of 365.py
+++ Day 37 of 365.py
@@ -5,3 +5,14 @@ Created on Thu Jun  3 09:21:46 2021
 @author: Atul Mishra
 """
 
+'''
+Append and Delete
+
+You have two strings of lowercase English letters. You can perform two types of operations on the first string:
+
+    1. Append a lowercase English letter to the end of the string.
+    2. Delete the last character of the string. Performing this operation on an empty string results in an empty string.
+
+Given an integer,k, and two strings, s and t, determine whether or not you can convert s to t by performing exactly k of the above operations on s. If it's possible, print Yes. Otherwise, print No.
+'''
+
