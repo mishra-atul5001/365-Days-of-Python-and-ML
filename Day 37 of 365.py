0763463695132170d20b165ab70825a6ac816687
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-03 10:06:08 +0530

    Day 37 of 365 days of Python and ML
    Append and Delete

--- Day 37 of 365.py
+++ Day 37 of 365.py
@@ -16,3 +16,22 @@ You have two strings of lowercase English letters. You can perform two types of 
 Given an integer,k, and two strings, s and t, determine whether or not you can convert s to t by performing exactly k of the above operations on s. If it's possible, print Yes. Otherwise, print No.
 '''
 
+
+def Append_and_Delete(s,t,k):
+    my_s = [str(char) for char in s]
+    my_t = [str(char) for char in t]
+    
+    my_instersections = set(my_t).intersection(my_s)
+    print(my_instersections)
+    print(len(my_instersections))
+    
+    if len(my_instersections) > k:
+        print('No')
+    elif len(my_t) - len(my_instersections) - 1 <= k:
+        print('Yes')
+    else:
+        print('Yes')
+        
+Append_and_Delete('hackerhappy','hackerrank',9)
+Append_and_Delete('ashley','ash',2)
+Append_and_Delete('zzzzz','zzzzzzz',4)
