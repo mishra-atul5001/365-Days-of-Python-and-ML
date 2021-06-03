299a3c0ad597ea3ad53e7d9d046bb95dd7d53e11
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-03 09:31:48 +0530

    Adding files
    Day 37 of 365
    Day 38 of 365
    Day 39 of 365

---
+++ Day 37 of 365.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 09:21:46 2021
+
+@author: Atul Mishra
+"""
+
---
+++ Day 39 of 365.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 09:22:23 2021
+
+@author: Atul Mishra
+"""
+
