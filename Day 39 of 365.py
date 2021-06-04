00a5db3e7c06840e15de9e85ba0c85b5ee0bfd8a
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-04 08:56:24 +0530

    Day 39 of 365 days of Python and ML
    MaxProductOfThree

--- Day 39 of 365.py
+++ Day 39 of 365.py
@@ -5,3 +5,43 @@ Created on Thu Jun  3 09:22:23 2021
 @author: Atul Mishra
 """
 
+'''
+MaxProductOfThree
+
+A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
+
+For example, array A such that:
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+
+contains the following example triplets:
+
+        (0, 1, 2), product is −3 * 1 * 2 = −6
+        (1, 2, 4), product is 1 * 2 * 5 = 10
+        (2, 4, 5), product is 2 * 5 * 6 = 60
+
+Your goal is to find the maximal product of any triplet.
+
+Write a function:
+
+    class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A, returns the value of the maximal product of any triplet.
+
+For example, given array A such that:
+  A[0] = -3
+  A[1] = 1
+  A[2] = 2
+  A[3] = -2
+  A[4] = 5
+  A[5] = 6
+
+the function should return 60, as the product of triplet (2, 4, 5) is maximal.
+'''
+
+
+
