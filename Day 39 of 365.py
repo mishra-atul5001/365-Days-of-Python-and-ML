3bb1ecdd75a90190af59c448dfc5cbdcd7759859
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-04 09:02:06 +0530

    Day 39 of 365 days of Python and ML

--- Day 39 of 365.py
+++ Day 39 of 365.py
@@ -42,6 +42,21 @@ For example, given array A such that:
 
 the function should return 60, as the product of triplet (2, 4, 5) is maximal.
 '''
+def MaxProductOfThree(list_to_check):
+    
+    '''
+    This problem is more easy if we just use some MATHS.
+    Step 1: Sort the LIST
+    Step 2: Max product can only be achieved if the FIRST 2 negatives are multiplied with the LARGEST element and LAST 3 elements are multiplied.
+    In this manner only, we can get the LARGEST PRODUCT.
+    
+    Ex: [-3,-2,2,1,4,5] -> If we sort if then [-3,-2,1,2,4,5] -> So two products will be MAX((-3*-2*5),(2*4*5)) -> (2*4*5) -> 40 
+    '''
+    list_to_check.sort()
+    
+    count = max(list_to_check[0]*list_to_check[1]*list_to_check[-1] , list_to_check[-1]*list_to_check[-2]*list_to_check[-3])
+    return count
 
+print(MaxProductOfThree([-3,-2,2,1,4,5]))
 
-
+print(MaxProductOfThree([-3,1,2,-2,5,6]))
