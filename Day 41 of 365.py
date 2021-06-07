11b667a1f4555b3e20f0b3e8db80ab5bb998bd29
Atul Mishra <atul.mishra5001@gmail.com>
2021-06-07 09:47:02 +0530

    Adding Skeleton Files

---
+++ Day 41 of 365.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 09:45:44 2021
+
+@author: Atul Mishra
+"""
+
---
+++ Day 42 of 365.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 09:45:57 2021
+
+@author: Atul Mishra
+"""
+
